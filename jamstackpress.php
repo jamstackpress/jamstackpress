170245c992a37f94e95b9c7ca3eaac20a999cf51
guiman87 <guiman.uy@gmail.com>
2021-03-25 16:37:30 -0300

    Freemius Integration

--- jamstackpress.php
+++ jamstackpress.php
@@ -18,6 +18,46 @@
  * Text Domain:       jamstack-press
  */
 
+
+ /**
+ * Freemius Integration.
+ * 
+ * @see https://dashboard.freemius.com/#!/live/plugins/8016/integration/
+ */
+if ( ! function_exists( 'jam_fs' ) ) {
+    // Create a helper function for easy SDK access.
+    function jam_fs() {
+        global $jam_fs;
+
+        if ( ! isset( $jam_fs ) ) {
+            // Include Freemius SDK.
+            require_once dirname(__FILE__) . '/src/freemius/start.php';
+
+            $jam_fs = fs_dynamic_init( array(
+                'id'                  => '8016',
+                'slug'                => 'jamstackpress',
+                'type'                => 'plugin',
+                'public_key'          => 'pk_444c369102867988cad4363ec8c6d',
+                'is_premium'          => false,
+                'has_addons'          => false,
+                'has_paid_plans'      => false,
+                'menu'                => array(
+                    'slug'           => 'jamstackpress',
+                    'account'        => false,
+                    'support'        => false,
+                ),
+            ) );
+        }
+
+        return $jam_fs;
+    }
+
+    // Init Freemius.
+    jam_fs();
+    // Signal that SDK was initiated.
+    do_action( 'jam_fs_loaded' );
+}
+
 if (! defined( 'ABSPATH' ) ) {
     exit;
 }
