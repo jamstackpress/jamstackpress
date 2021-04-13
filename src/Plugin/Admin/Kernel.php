114993ea230051870b36b9bce3c6fc1e81764bce
Santiago Cajal <s.cajalvarela@gmail.com>
2021-04-13 12:40:23 -0300

    admin kernel

---
+++ src/Plugin/Admin/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace JamstackPress\Admin;
+
+class Kernel
+{
+    /**
+     * Boot the administration kernel.
+     * 
+     * @return void
+     */
+    public static function boot()
+    {
+        
+    }
+}
